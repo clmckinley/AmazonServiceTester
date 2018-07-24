7b48e09c92c3245c87ad7822bd33c8aecb71466b
Logan McKinley <clm8v@virginia.edu>
2018-07-24 09:50:35 -0400

    added s3 data store

--- AmazonServiceTester/Models/Settings.cs
+++ AmazonServiceTester/Models/Settings.cs
@@ -10,5 +10,12 @@ namespace AmazonServiceTester.Models
     {
         public string CreateAccountUrl { get; set; }
         public string GetCredentialsUrl { get; set; }
+        public int WriteInterval { get; set; } = 1; //write every 1 minute by default
+        public int WriteCount { get; set; } = 15;
+        public string DeviceId { get; set; }
+        public string ParticipantId { get; set; }
+        public string S3Region { get; set; } = "us-east-1";
+        public string S3Bucket { get; set; } = "bucketId";
+        public int S3CancellationTime { get; set; } = 60;
     }
 }
--- AmazonServiceTester/Program.cs
+++ AmazonServiceTester/Program.cs
@@ -1,11 +1,14 @@
 ﻿using AmazonServiceTester.Models;
 using Newtonsoft.Json;
+using Sensus.DataStores.Remote;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AmazonServiceTester
@@ -16,22 +19,25 @@ namespace AmazonServiceTester
         {
             var restClient = new HttpClient();
             var settingsPath = args.Count() > 0 ? args[0] : Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "settings.json");
+            var curWriteCnt = 0;
+            AmazonS3RemoteDataStore s3Store = null;
             Settings settings = new Settings();
             try
             {
                 var consoleBreakText = "================\n\n\n";
-                var deviceId = $"console-test-{DateTime.Now.Ticks.ToString("x")}";
-                var participantId = $"{deviceId}-user{DateTime.Now.Ticks.ToString("x")}";
                 while (string.IsNullOrWhiteSpace(settingsPath) || File.Exists(settingsPath) == false)
                 {
                     Console.Write("Enter settings path:");
                     settingsPath = Console.ReadLine();
                 }
                 Console.WriteLine($"Loading Settings from {settingsPath}");
-                settings = JsonConvert.DeserializeObject<Settings>(File.ReadAllText(settingsPath));
+                settings = JsonConvert.DeserializeObject<Settings>(File.ReadAllText(settingsPath), new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Include});
+                settings.DeviceId = settings.DeviceId ?? $"console-test-{DateTime.Now.Ticks.ToString("x")}";
+                settings.ParticipantId = settings.ParticipantId ?? $"{settings.DeviceId}-user{DateTime.Now.Ticks.ToString("x")}";
+
                 Console.Write(consoleBreakText);
 
-                var createAccountUrl = string.Format(settings.CreateAccountUrl, participantId, deviceId);
+                var createAccountUrl = string.Format(settings.CreateAccountUrl, settings.ParticipantId, settings.DeviceId);
                 var account = await DoGet<Account>(createAccountUrl, restClient);
                 Console.WriteLine($"\t got {account}");
                 Console.Write(consoleBreakText);
@@ -41,9 +47,58 @@ namespace AmazonServiceTester
                 Console.WriteLine($"\t got {credentials}");
                 Console.Write(consoleBreakText);
 
+                if (string.IsNullOrWhiteSpace(credentials.protocolURL) == false)
+                {
+                    var protocolBytes = await DoDownload(credentials.protocolURL, restClient);
+                    //TODO:  Load the new protocol
+                }
+                else
+                {
+                    Console.WriteLine("Protocol not sent with credentials");
+                }
+                Console.Write(consoleBreakText);
+
+
+                if (settings.WriteCount == 0)
+                {
+                    Console.WriteLine("Not configured to write anything to S3 so exiting");
+                }
+                else
+                {
+                    while (curWriteCnt < settings.WriteCount)
+                    {
+                        if(curWriteCnt > 0)
+                        {
+                            Console.WriteLine($"Sleeping for {settings.WriteInterval} seconds");
+                            Console.Write(consoleBreakText);
+                            System.Threading.Thread.Sleep(settings.WriteInterval * 1000); //convert the minutes into ms
+                        }
+
+                        Console.WriteLine($"Sending file {curWriteCnt}/{settings.WriteCount}");
+                        var utcExpiration = new DateTimeOffset(long.Parse(credentials.expiration), new TimeSpan(0));
+                        if (utcExpiration <= DateTimeOffset.UtcNow)
+                        {
+                            Console.WriteLine("Credentials expired so getting new");
+                            credentials = await DoGet<AccountCredentials>(getCredentialsUrl, restClient);
+                            s3Store = null;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Credentials still good");
+                        }
+                        s3Store = s3Store ?? GetS3Store(settings, credentials);
+                        var toUploadString = $"Test upload for {account.participantId} at {DateTimeOffset.UtcNow.Ticks}";
+                        await UploadToS3(toUploadString, s3Store, settings, restClient);
+                        curWriteCnt++;
+                    }
+                }
+
+
+
+
 
             }
-            catch(Exception exc)
+            catch (Exception exc)
             {
                 Console.WriteLine("We got an error:" + exc.Message);
             }
@@ -53,8 +108,8 @@ namespace AmazonServiceTester
                 restClient = null;
             }
             Console.WriteLine("Press any key to exit");
-            var k = Console.ReadKey();           
-            if(k.KeyChar == 's')
+            var k = Console.ReadKey();
+            if (k.KeyChar == 's')
             {
                 File.WriteAllText(settingsPath, JsonConvert.SerializeObject(settings));
             }
@@ -69,11 +124,67 @@ namespace AmazonServiceTester
                 Console.WriteLine($"\tGot: {responseTxt}");
                 return JsonConvert.DeserializeObject<T>(responseTxt);
             }
-            catch(Exception exc)
+            catch (Exception exc)
             {
                 Console.WriteLine($"Error doing Get.  msg:{exc.Message}responseTxt:{responseTxt}");
                 throw;
             }
         }
+
+        static async Task<byte[]> DoDownload(string url, HttpClient client)
+        {
+            byte[] responseBytes = null;
+            try
+            {
+                Console.WriteLine($"downloading {url}");
+                responseBytes = await client.GetByteArrayAsync(url);
+                if (responseBytes == null || responseBytes.LongLength == 0)
+                {
+                    throw new Exception("Nothing downloaded");
+                }
+                Console.WriteLine($"\tGot: {responseBytes.LongLength} bytes");
+                return responseBytes;
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine($"Error doing Download.  msg:{exc.Message}");
+                throw;
+            }
+        }
+        static AmazonS3RemoteDataStore GetS3Store(Settings settings, AccountCredentials credentials)
+        {
+            AmazonS3RemoteDataStore s3Store = new AmazonS3RemoteDataStore();
+            s3Store.Bucket = settings.S3Bucket;
+            s3Store.ParticipantId = settings.ParticipantId;
+            s3Store.DeviceId = settings.DeviceId;
+            s3Store.Region = settings.S3Region;
+            s3Store.IamAccountString = $"{credentials.accessKeyId}:{credentials.secretAccessKey}";
+            return s3Store;
+        }
+        static async Task<bool> UploadToS3(string contents, AmazonS3RemoteDataStore s3Store, Settings settings, HttpClient client)
+        {
+            try
+            {
+                Console.WriteLine($"Writing {contents} to S3");
+                var cancellationToken = new CancellationTokenSource(settings.S3CancellationTime*60*1000).Token;
+                cancellationToken.ThrowIfCancellationRequested();
+                await s3Store.WriteStringAsync(contents, cancellationToken);
+                Console.WriteLine("\t Write finished");
+                var rVal = await s3Store.GetDatumAsync(s3Store.GetDatumKey(contents), cancellationToken);
+                Console.WriteLine($"\t Read {rVal}");
+                if(rVal != contents)
+                {
+                    throw new Exception("write/read don't match");
+                }
+
+                return true;
+            }
+            catch(Exception exc)
+            {
+                Console.WriteLine("Write failed. Msg:" + exc.Message);
+                return false;
+            }
+        }
+
     }
 }
